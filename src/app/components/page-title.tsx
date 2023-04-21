1f6a76041c7ded5092684164ba2045fd2aeeb769
flofehrenbacher-sz <florian.fehrenbacher@sz.de>
2023-04-21 11:18:54 +0200

    feat: add wavy underline

--- src/app/components/page-title.tsx
+++ src/app/components/page-title.tsx
@@ -1,5 +1,35 @@
 export function PageTitle({ children }: { children: string }) {
   return (
-    <h1 className="text-4xl inline border-cyan-600 border-b-4">{children}</h1>
+    <div className="isolate relative flex flex-col items-center">
+      <h1 className="text-4xl inline">{children}</h1>
+      <Wave className="relative -top-3 -z-10" />
+    </div>
+  );
+}
+
+function Wave({ className }: { className: string }) {
+  return (
+    <svg viewBox="3 19 100 18" className={className}>
+      <path
+        d="M0,25 
+  C25,10 25,40 50,25 
+  S75,40 100,25 
+  S125,10 150,25 
+  S175,40 200,25"
+        className="stroke-blue-700"
+        stroke-width="3"
+        fill="none"
+      />
+      <path
+        d="M0,25 
+  C25,10 25,40 50,25 
+  S75,40 100,25 
+  S125,10 150,25 
+  S175,40 200,25"
+        className="stroke-blue-400 translate-y-1"
+        stroke-width="3"
+        fill="none"
+      />
+    </svg>
   );
 }
